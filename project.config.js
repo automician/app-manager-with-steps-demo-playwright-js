5f796b5ac6fbf677282046224e38776b2111a87e
yashaka <yashaka@gmail.com>
2024-09-02 20:27:52 +0300

    NEW: add playwright sub-config to project config ...
    + ignore in withEnvOverrides
    + fix expectations withMatcherasSteps to work on canceled withSteps + corresponding refactoring
    + fix extended expect usage

--- project.config.js
+++ project.config.js
@@ -1,6 +1,14 @@
 import withEnvOverrides from './lib/support/config/withEnvOverrides.js'
+import playwrightConfig from './playwright.config.js'
 
-export const config = withEnvOverrides({
-  disableObjectSteps: false,
-  enableMatcherSteps: false,
-})
+export const config = withEnvOverrides(
+  {
+    cancelWithSteps: false,
+    enableMatcherSteps: false,
+    /* ⬇️ sub-configs ⬇️ */
+    playwright: playwrightConfig,
+  },
+  {
+    ignore: ['playwright'],
+  },
+)
